e4e0f592f2424157fd37efeb8a093e71f3ec7601
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-14 15:22:38 +0300

    finished episode 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
 use App\Models\User;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,80 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user1 = User::factory()->create();
-        $user2 = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $bob = User::factory()->create([
+            'name' => 'Bob Legit'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
+        $alice = User::factory()->create([
+            'name' => 'Alice Legit'
         ]);
 
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies',
+        $charlie = User::factory()->create([
+            'name' => 'Charlie The Bad Guy'
         ]);
 
-        Post::create([
-            'user_id' => $user1->id,
-            'category_id' => $personal->id,
-            'title' => 'My first post',
-            'slug' => 'my-first-post',
-            'body' => '
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque elementum, rhoncus libero ac, ullamcorper urna. Nulla eget mi consequat, elementum justo eu, suscipit ante. Vivamus varius sem id lobortis commodo. Quisque eget est et ipsum pulvinar ultrices at ac massa. Duis ante risus, auctor eget magna ut, tristique sollicitudin tellus. Curabitur ac massa aliquam quam semper fringilla. Sed sit amet elementum ex.
-            
-            Maecenas hendrerit, orci vel elementum ultricies, est leo vestibulum sem, at suscipit ex est non nibh. Aliquam neque orci, volutpat nec magna vitae, efficitur semper nisl. Nulla facilisi. Nam facilisis felis eget ornare fringilla. Integer ac erat nibh. Cras id diam venenatis, gravida odio a, consectetur risus. Praesent in ipsum dui. Fusce volutpat arcu justo, nec sodales sapien tempus at. Donec sollicitudin diam at sodales efficitur.
-            
-            Pellentesque quis ornare erat. Suspendisse vitae libero malesuada, varius metus id, fringilla neque. Curabitur maximus gravida turpis, ut euismod neque tempor nec. Sed vel blandit lorem. Integer pulvinar ex odio, id dignissim elit convallis eget. Suspendisse finibus tincidunt nibh eu imperdiet. Fusce euismod massa in quam dictum volutpat.
-            
-            Vestibulum et bibendum leo, sit amet rutrum quam. Nullam cursus tincidunt egestas. Donec ligula arcu, posuere ac malesuada eu, blandit eu orci. ',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-
+        Post::factory(3)->create([
+            'user_id' => $bob->id
         ]);
 
-        Post::create([
-            'user_id' => $user2->id,
-            'category_id' => $work->id,
-            'title' => 'My second post',
-            'slug' => 'my-second-post',
-            'body' => '
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque elementum, rhoncus libero ac, ullamcorper urna. Nulla eget mi consequat, elementum justo eu, suscipit ante. Vivamus varius sem id lobortis commodo. Quisque eget est et ipsum pulvinar ultrices at ac massa. Duis ante risus, auctor eget magna ut, tristique sollicitudin tellus. Curabitur ac massa aliquam quam semper fringilla. Sed sit amet elementum ex.
-            
-            Maecenas hendrerit, orci vel elementum ultricies, est leo vestibulum sem, at suscipit ex est non nibh. Aliquam neque orci, volutpat nec magna vitae, efficitur semper nisl. Nulla facilisi. Nam facilisis felis eget ornare fringilla. Integer ac erat nibh. Cras id diam venenatis, gravida odio a, consectetur risus. Praesent in ipsum dui. Fusce volutpat arcu justo, nec sodales sapien tempus at. Donec sollicitudin diam at sodales efficitur.
-            
-            Pellentesque quis ornare erat. Suspendisse vitae libero malesuada, varius metus id, fringilla neque. Curabitur maximus gravida turpis, ut euismod neque tempor nec. Sed vel blandit lorem. Integer pulvinar ex odio, id dignissim elit convallis eget. Suspendisse finibus tincidunt nibh eu imperdiet. Fusce euismod massa in quam dictum volutpat.
-            
-            Vestibulum et bibendum leo, sit amet rutrum quam. Nullam cursus tincidunt egestas. Donec ligula arcu, posuere ac malesuada eu, blandit eu orci. ',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            
+        Post::factory(3)->create([
+            'user_id' => $alice->id
         ]);
 
-        Post::create([
-            'user_id' => $user1->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My third post',
-            'slug' => 'my-third-post',
-            'body' => '
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque elementum, rhoncus libero ac, ullamcorper urna. Nulla eget mi consequat, elementum justo eu, suscipit ante. Vivamus varius sem id lobortis commodo. Quisque eget est et ipsum pulvinar ultrices at ac massa. Duis ante risus, auctor eget magna ut, tristique sollicitudin tellus. Curabitur ac massa aliquam quam semper fringilla. Sed sit amet elementum ex.
-            
-            Maecenas hendrerit, orci vel elementum ultricies, est leo vestibulum sem, at suscipit ex est non nibh. Aliquam neque orci, volutpat nec magna vitae, efficitur semper nisl. Nulla facilisi. Nam facilisis felis eget ornare fringilla. Integer ac erat nibh. Cras id diam venenatis, gravida odio a, consectetur risus. Praesent in ipsum dui. Fusce volutpat arcu justo, nec sodales sapien tempus at. Donec sollicitudin diam at sodales efficitur.
-            
-            Pellentesque quis ornare erat. Suspendisse vitae libero malesuada, varius metus id, fringilla neque. Curabitur maximus gravida turpis, ut euismod neque tempor nec. Sed vel blandit lorem. Integer pulvinar ex odio, id dignissim elit convallis eget. Suspendisse finibus tincidunt nibh eu imperdiet. Fusce euismod massa in quam dictum volutpat.
-            
-            Vestibulum et bibendum leo, sit amet rutrum quam. Nullam cursus tincidunt egestas. Donec ligula arcu, posuere ac malesuada eu, blandit eu orci. ',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            
+        Post::factory(3)->create([
+            'user_id' => $charlie->id
         ]);
     }
 }
