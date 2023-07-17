3e3d1af4bf87887e03c38816a5caaab4d162bb8a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-17 12:21:03 +0300

    finished episode 54

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Comment;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -34,6 +35,11 @@ class Post extends Model
                 $query->where('username', $author)
             )
         );
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function category()
